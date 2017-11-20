2ee71bb25ee0ebbdc7d3bbc006cf8b4bdd363c41
Pengge Zhuang <1064480036@qq.com>
2017-11-20 18:27:14 +0800

    init php7_kernel_module_translate

---
+++ php7_kernel_module_example_of_sizeof/cmake-build-debug/CMakeFiles/php7_kernel_module_example_of_sizeof.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/php7_kernel_module_example_of_sizeof.dir/main.c.o"
+  "php7_kernel_module_example_of_sizeof.pdb"
+  "php7_kernel_module_example_of_sizeof"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/php7_kernel_module_example_of_sizeof.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
