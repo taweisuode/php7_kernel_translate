e4c35ba8adb3704d2de3b82ab321a62090bb090e
Pengge Zhuang <1064480036@qq.com>
2017-11-23 23:54:48 +0800

    add:php hash

--- php7_kernel_module_example_of_sizeof/hash.c
+++ php7_kernel_module_example_of_sizeof/hash.c
@@ -11,4 +11,16 @@ int hashMain() {
     printf("result = %zu\n",strlen(a));
     printf("result = %d\n",time33((const unsigned char *) b, (int) strlen(b)));
 
+    //php版本hash函数
+    /**
+        $hash = 5381;
+        $s    = md5($retParams['key']); //相比其它版本，进行了md5加密
+        $seed = 5;
+        $len  = 32;//加密后长度32
+        for ($i = 0; $i < 32; $i++) {
+            $hash = ($hash << $seed) + $hash + ord($s{$i});
+        }
+        echo $hash & 0x7FFFFFFF;die;
+     */
+
 }
